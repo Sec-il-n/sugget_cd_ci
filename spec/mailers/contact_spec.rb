08e692522a4d8f9f2cc4758300bd4594a8f77710
secil <loveuallnbs0205@yaho.co.jp>
2021-02-08 20:30:15 +0900

    お問い合わせ機能ActionMailer mailer_spec

--- spec/mailers/contact_spec.rb
+++ spec/mailers/contact_spec.rb
@@ -1,5 +1,31 @@
 require "rails_helper"
 
 RSpec.describe ContactMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:contact) { create(:contact, user_id: user.id) }
+
+  describe 'お問い合わせ機能' do
+    context 'お問い合わせ内容が作成された場合' do
+      # Ⓜ︎system specではない→User単体なのでここでのUserとの紐づき確認なし
+      # before do
+      #   login(user)
+      # end
+      
+      # Mailerクラスのメソッドを実行
+      subject(:mail) do
+        # described_class(describeのクラスで宣言されあtクラスを取得)＝ContactMailer
+        described_class.contact_mail(contact).deliver_now
+        ActionMailer::Base.deliveries.last
+      end
+      after(:all) do
+        ActionMailer::Base.deliveries.clear
+      end
+
+      it '管理者に問い合わせのメールが届く' do
+        expect(mail.to.first).to eq('kannrisya@co.jp')
+        expect(mail.subject).to eq('問い合わせがありました')
+        expect(mail.body).to match("#{user.email}")
+      end
+    end
+  end
 end
