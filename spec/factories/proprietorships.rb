f7b52bd701ab14990157d1a07c255588062950dd
secil <loveuallnbs0205@yahoo.co.jp>
2021-02-11 04:23:08 +0900

    seed.rb, FactoryBot修正

--- spec/factories/proprietorships.rb
+++ spec/factories/proprietorships.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :proprietorship do
     name { Faker::Company.name }
-    info { Faker::Lorem.characters(600) }# max 1000
+    info { Faker::Lorem.characters(number: 600) }# max 1000
     category_id { 1 }
   end
 end
