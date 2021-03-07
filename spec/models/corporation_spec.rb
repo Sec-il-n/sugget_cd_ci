df4eb62430777decaf723ce7b8e81c7603eb3ba2
secil <>
2021-03-07 14:23:27 +0900

    model_corporation spec全部間違い→修正

--- spec/models/corporation_spec.rb
+++ spec/models/corporation_spec.rb
@@ -1,21 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
-  describe 'お問い合わせ機能' do
-    let!(:user) { create(:user) }
-    let!(:contact) { create(:contact, user_id: user.id) }
-    context'お問い合わせ内容が未入力の時' do
+RSpec.describe Corporation, type: :model do
+  describe '企業登録機能' do
+    context'企業名が未入力の時' do
       it 'バリデーションに引っかかる' do
-        contact = create(:contact, content: '')
-        contact.valid?
-        expect(contact.errors.full_messages).to include('お問い合わせ内容を入力してください')
+        corporation = build(:corporation, name: '')
+        corporation.valid?
+        expect(corporation.errors.full_messages).to include('企業名を入力してください')
       end
     end
-    context'お問い合わせ内容が600字以上の時' do
+    context'詳細が未入力の時' do
       it 'バリデーションに引っかかる' do
-        contact = build(:contact, content: Faker::String.random(length: 601))
-        contact.valid?
-        expect(contact.errors.full_messages).to include('お問い合わせ内容は600文字以内で入力してください')
+        corporation = build(:corporation, info: '')
+        corporation.valid?
+        expect(corporation.errors.full_messages).to include('詳細を入力してください')
+      end
+    end
+    context'カテゴリーが未選択の時' do
+      it 'バリデーションに引っかかる' do
+        corporation = build(:corporation, category_id: nil)
+        corporation.valid?
+        expect(corporation.errors.full_messages).to include('Categoryを入力してください')
+      end
+    end
+    context'企業名が20字以上の時' do
+      it 'バリデーションに引っかかる' do
+        corporation = build(:corporation, name: Faker::String.random(length: 601))
+        corporation.valid?
+        expect(corporation.errors.full_messages).to include('企業名は20文字以内で入力してください')
+      end
+    end
+    context'詳細が2000字以上の時' do
+      it 'バリデーションに引っかかる' do
+        corporation = build(:corporation, info: Faker::String.random(length: 2001))
+        corporation.valid?
+        expect(corporation.errors.full_messages).to include('詳細は2000文字以内で入力してください')
       end
     end
   end
