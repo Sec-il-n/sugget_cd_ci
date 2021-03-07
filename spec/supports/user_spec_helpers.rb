4189f2f1668529c9b74611c51e007b924a2aa86b
secil <>
2021-03-07 10:12:44 +0900

    login部分の不具合か

--- spec/supports/user_spec_helpers.rb
+++ spec/supports/user_spec_helpers.rb
@@ -3,17 +3,20 @@ module UserSpecHelpers
     user.confirm# factoryの:userのみ(letで:userと定義 x)
 
     visit new_user_session_path
-        fill_in "#{I18n.t('.dictionary.words.email')}", with: user.email
-        fill_in "#{I18n.t('.dictionary.words.password')}" , with: user.password
-        click_button "#{I18n.t('.dictionary.words.sign_in')}"
+    find('#user_email').fill_in with: user.email
+    find('#user_password').fill_in with: user.password
+    find(:xpath, '//*[@id="new_user"]/div[4]/input').click
+    # fill_in "#{I18n.t('.dictionary.words.email')}", with: user.email
+    # fill_in "#{I18n.t('.dictionary.words.password')}" , with: user.password
+    # click_button "#{I18n.t('.dictionary.words.sign_in')}"
   end
   def login(user)
     user.confirm# factoryの:userのみ(letで:userと定義 x)
 
     visit new_user_session_path
-        fill_in "#{I18n.t('.dictionary.words.email')}", with: user.email
-        fill_in "#{I18n.t('.dictionary.words.password')}" , with: user.password
-        click_button "#{I18n.t('.dictionary.words.sign_in')}"
+    find('#user_email').fill_in with: user.email
+    find('#user_password').fill_in with: user.password
+    find(:xpath, '//*[@id="new_user"]/div[4]/input').click
   end
   def login_admin(admin)
     sign_in users(admin), scope: :admin
