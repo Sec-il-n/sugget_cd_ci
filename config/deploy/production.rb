5fbe3e2fe9b915ed04a60892494a2d290d1a2098
secil <>
2021-03-10 08:53:27 +0900

    ssh-agent にpem登録

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,5 @@
 server '35.72.239.105', user: 'app', roles: %w{app db web}
-# set :ssh_options, keys: '~/.ssh/suggest_cd_ci-key.pem'
-# set :ssh_options, auth_methods: ['publickey'],
-                  # keys: '~/.ssh/cd_ci_rsa'# loca-aws ssh
+# set :ssh_options, keys: '~/.ssh/id_rsa'
 set :ssh_options, {
   keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true,
