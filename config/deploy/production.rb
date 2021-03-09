0a0ec521c414abe4d73c6cd6bdf27667538bec50
secil <>
2021-03-10 01:09:55 +0900

    circleci add-ssh-key 変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server '35.72.239.105', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '~/.ssh/suggest_cd_ci-key.pem'
-# set :ssh_options, keys: '/Users/secil/.ssh/id_rsa'
+# set :ssh_options, keys: '/Users/secil/.ssh/id_rsa'# loca-aws ssh
