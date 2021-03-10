f378eeb95f5fa13123acb786092ad2faf29e2ff5
secil <>
2021-03-11 02:03:14 +0900

    deploy.rb ssh_options ファイルpath変更(circle_ciではid_rsaで登録されている)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,13 @@
 server '35.72.239.105', user: 'app', roles: %w{app db web}
 # set :ssh_options, keys: '~/.ssh/id_rsa'
-set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/id_rsa']
+# set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/id_rsa']
 # set :ssh_options, {
 #   keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
 #   forward_agent: true,
 #   auth_methods: %w[publickey]
 # }
+set :ssh_options, {
+  keys: ['~/.ssh/cd_ci_rsa'],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
