710fdf87adbf0e3f9de71ec9f85689479178e952
secil <>
2021-03-10 22:15:34 +0900

    set options変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,8 @@
 server '35.72.239.105', user: 'app', roles: %w{app db web}
 # set :ssh_options, keys: '~/.ssh/id_rsa'
-set :ssh_options, {
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
-  forward_agent: true,
-  auth_methods: %w[publickey]
-}
+set :ssh_options, keys: '~/.ssh/cd_ci_rsa'
+# set :ssh_options, {
+#   keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+#   forward_agent: true,
+#   auth_methods: %w[publickey]
+# }
