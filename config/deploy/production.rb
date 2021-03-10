c8d640d591e19708d534d21c2af76f62f81f04f3
secil <>
2021-03-11 02:37:37 +0900

    deploy.rb masterkey シンボリック　除去

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,15 @@
 server '35.72.239.105', user: 'app', roles: %w{app db web}
 # set :ssh_options, keys: '~/.ssh/id_rsa'
 # set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/id_rsa']
-# set :ssh_options, {
-#   keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
-#   forward_agent: true,
-#   auth_methods: %w[publickey]
-# }
 set :ssh_options, {
-  user: 'app',
-  keys: ['~/.ssh/cd_ci_rsa'],
+  port: 22,
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true,
-  auth_methods: ['publickey']
+  auth_methods: %w[publickey]
 }
+# set :ssh_options, {
+#   user: 'app',
+#   keys: ['~/.ssh/cd_ci_rsa'],
+#   forward_agent: true,
+#   auth_methods: ['publickey']
+# }
